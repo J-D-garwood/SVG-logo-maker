a5fbcdf48526e88c7510fbabb5e590664629751b
J_D_Nye <garwoodjdm@gmail.com>
2023-07-27 09:45:01 +1000

    Initial base functionality established

--- Main/index.js
+++ Main/index.js
@@ -1,7 +1,12 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+const shapes = require('../Main/lib/shapes.js')
 
-const generateSVG = (shape) =>
+const generateSVG = (shape) => 
+    `<svg width="500" height="500" version="1.1" xmlns="http://www.w3.org/2000/svg">
+      ${shape.render()}
+    </svg>`;
+
 
 inquirer
     .prompt([
@@ -27,6 +32,20 @@ inquirer
             message: 'Enter shape color:'
         },
     ])
-    .then((answers) => {
-        
+    .then((res) => {
+        if (res.shape === 'circle') {
+            const testshape = new shapes.circle(res.text, res.textCol, res.shapeCol)
+            return testshape
+        } else if (res.shape === 'triangle') {
+            const testshape = new shapes.triangle(res.text, res.textCol, res.shapeCol)
+            return testshape
+        } else {
+            const testshape = new shapes.square(res.text, res.textCol, res.shapeCol)
+            return testshape
+        }
+    }).then((newshape) => {
+        const pageContent = generateSVG(newshape)
+
+        fs.writeFile('./examples/logo.svg', pageContent, (error) =>
+        error ? console.log(error) : console.log('Successfully created logo.html!'))
     })
--- Main/lib/shapes.js
+++ Main/lib/shapes.js
@@ -1,35 +1,39 @@
 class shape {
-    constructor(text, textCol, col) {
+    constructor(text = "SVG", textCol = "white", col = "green") {
+        if (text.length>3) {
+            throw new Error('Logo text can not be longer than 3 characters');
+          };
         this.text = text;
         this.textCol = textCol;
         this.col = col;
+        this.form = ''
+    }
+    render() {
+        return this.form
+    }
+    setColor(newCol) {
+        this.col = newCol
     }
 }
 
 class square extends shape {
     constructor(text, textCol, col) {
         super(text, textCol, col)
-    }
-    render() {
-        
+        this.form = `<rect x="0" y="0" width="500" height="500" stroke="black" fill="${col}" stroke-width="1">`
     }
 }
-
+//<rect x="0" y="0" width="500" height="500" stroke="black" fill="green" stroke-width="1"></rect>
 class circle extends shape {
     constructor(text, textCol, col) {
         super(text, textCol, col)
-    }
-    render() {
-        
+        this.form = `<circle cx="250" cy="250" r="250" stroke="black" fill="${col}" stroke-width="1">`
     }
 }
 
 class triangle extends shape {
     constructor(text, textCol, col) {
         super(text, textCol, col)
-    }
-    render() {
-        
+        this.form = `<polygon points="200 15, 400 400, 0 400" fill="${col}">`
     }
 }
 
