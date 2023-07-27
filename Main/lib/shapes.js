1f247f90d2c36c01c4033d344d312c066ad6294c
J_D_Nye <garwoodjdm@gmail.com>
2023-07-28 09:32:58 +1000

    started working on tests

--- Main/lib/shapes.js
+++ Main/lib/shapes.js
@@ -13,39 +13,44 @@ class shape {
     }
     displaytext() {
         if (this.text.length===3) {
-            return `<text x="75" y="300" font-family="Times New Roman" font-size="160" fill="${this.textCol}">${this.text}</text>`
+            return `<text x="75" y="300" font-family="Times New Roman" font-size="180" fill="${this.textCol}">${this.text}</text>`
         } else if (this.text.length===2) {
-            return `<text x="115" y="300" font-family="Times New Roman" font-size="160" fill="${this.textCol}">${this.text}</text>`
+            return `<text x="115" y="300" font-family="Times New Roman" font-size="180" fill="${this.textCol}">${this.text}</text>`
         } else {
-            return `<text x="190" y="300" font-family="Times New Roman" font-size="160" fill="${this.textCol}">${this.text}</text>`
+            return `<text x="190" y="300" font-family="Times New Roman" font-size="180" fill="${this.textCol}">${this.text}</text>`
         }
     }
     setColor(newCol) {
-        this.col = newCol
+        this.col = newCol;
+        console.log(this.col)
     }
 
 }
 
 class square extends shape {
-    constructor(text, textCol, col) {
+    constructor(text = "SVG", textCol = "white", col = "green") {
         super(text, textCol, col)
         this.form = `<rect x="0" y="0" width="500" height="500" stroke="black" fill="${col}" stroke-width="1"></rect>`
     }
 }
 //<rect x="0" y="0" width="500" height="500" stroke="black" fill="green" stroke-width="1"></rect>
 class circle extends shape {
-    constructor(text, textCol, col) {
+    constructor(text = "SVG", textCol = "white", col = "green") {
         super(text, textCol, col)
         this.form = `<circle cx="250" cy="250" r="250" stroke="black" fill="${col}" stroke-width="1"></circle>`
     }
 }
 
 class triangle extends shape {
-    constructor(text, textCol, col) {
+    constructor(text = "SVG", textCol = "white", col = "green") {
         super(text, textCol, col)
         this.form = `<polygon points="250 15, 500 400, 0 400" fill="${col}"></polygon>`
     }
 }
+
+const testShape = new triangle
+testShape.setColor('blue')
+console.log(triangle.col)
 
 module.exports = {
     shape,
---
+++ Main/tests/shapes.test.js
@@ -0,0 +1,13 @@
+const Shapes = require('../lib/shapes.js')
+
+describe('Shapes', () => {
+
+    describe('triangle render', () => {
+        it('should render the svg shape code applied to it in the svg file', () => {
+            const testShape = new Shapes.triangle();
+            testShape.setColor("blue");
+            expect(testShape.render()).toEqual(`<polygon points="250 15, 500 400, 0 400" fill="blue"></polygon>`);
+        })
+    })
+    //describe('triangle text displayed')
+})
